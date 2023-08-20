039ba2a61f6ec88cc17328a0cb4425443e80c447
legend23 <yu@vtechnology.ru>
2023-08-20 13:15:36 +0500

    fix

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ bot = telebot.TeleBot(os.getenv('TELEGRAMM_TOKEN'))
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    """Функция для теста работоспособности бота"""
     bot.send_message(message.chat.id, 'Привет')
 
 
 @bot.message_handler(content_types=['text'])
 def get_time_message(message):
+    """Обработка сообщений от пользователя на предмет игры в 'время'"""
     time_current_mesage = datetime.datetime.fromtimestamp(message.date).strftime('%H:%M')
-    print(time_current_mesage)
-    print(message.text.strip())
-    if time_current_mesage == message.text.strip():
-        if valid_time(message.text):
+    if valid_time(message.text):
+        if time_current_mesage == message.text.strip():
             score = parse_time(message.text)
             bot.send_message(message.chat.id, "Твой результат " + str(score))
-    else:
-        bot.send_message(message.chat.id, "Не успел?))))")
+        else:
+            bot.send_message(message.chat.id, "Не успел?))))")
 
 
 bot.polling(none_stop=True)
---
+++ sqltable.py
@@ -0,0 +1,47 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import  Column, Integer, String
+from sqlalchemy.orm import Session
+
+engine = create_engine("sqlite:///sql.db")
+
+
+class Base(DeclarativeBase): pass
+  
+class Player(Base):
+    __tablename__ = "players"
+  
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    score = Column(Integer)
+
+def create_tables():
+    # создаем таблицы
+    Base.metadata.create_all(bind=engine)
+
+
+def add_player(name, score):
+    # создаем сессию подключения к бд
+    with Session(autoflush=False, bind=engine) as db:
+        # создаем объект Person для добавления в бд
+        player = Player(name=name, score=score)
+        db.add(player)     # добавляем в бд
+        db.commit()     # сохраняем изменения
+
+def get_player_score(name):
+    # создаем сессию подключения к бд
+    with Session(autoflush=False, bind=engine) as db:
+        # создаем объект Person для добавления в бд
+        player = db.query(Player).filter(Player.name == name).first()
+        return player.score if player else None
+
+
+def change_player_score(name, score):
+    # создаем сессию подключения к бд
+    with Session(autoflush=False, bind=engine) as db:
+        # создаем объект Person для добавления в бд
+        player = db.query(Player).filter(Player.name == name).first()
+        if player:
+            player.score = score
+            db.commit()
