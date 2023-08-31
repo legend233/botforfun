e29ecd6f0b2e64bb117c6b870deb095946898733
legend23 <yu@vtechnology.ru>
2023-08-31 18:12:34 +0500

    add колпаков ию

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ def start_message(message):
     sort_top_players = sorted(top_players, key=lambda x: int(x[1]), reverse=True)
     scores = []
     for player, score in sort_top_players:
+        if player == "TestVtechnologyBOT":
+            player = "Колпаков И.Ю."
         scores.append(f"{player}: {score} {emoji[tier(score)]}")
     # отправляем gif анимацию и сообщение
     bot.send_animation(message.chat.id, open('images/top.gif', 'rb'), caption="\n".join(scores))
